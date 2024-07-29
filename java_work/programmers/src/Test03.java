d75d0d8f15297ee2c23a3ebdf6797643f5cab1c5
lia710 <hjlavender94@gmail.com>
2024-07-29 17:48:52 +0900

    0729

--- java_work/programmers/src/Test03.java
+++ java_work/programmers/src/Test03.java
@@ -1,6 +1,19 @@
-import java.util.Arrays;
-
-import static java.lang.System.arraycopy;
-import static java.util.Arrays.fill;
+class Test03 {
+    public int solution(int n) {
+        int a=1;
+        int b=1;
+        int count=0;
+        for (int i = 0; i < n; i++) {
+            a++;
+            b++;
+            if (a * b == n) {
+                System.out.println(a + "," + b);
+                count++;
+            }
+        }
+        int answer =count;
+        return answer;
+    }
+}
 
 
--- java_work/programmers/src/test04.java
+++ java_work/programmers/src/test04.java
@@ -1,25 +1,16 @@
 import java.util.Arrays;
 
 public class test04 {
-    public static void main(String[] args) {
-
-        Solution so = new Solution();
-        so.solution(10);
-
-
-    }
-    static class Solution{
-    public int[] solution ( int n){
-
-        int[] arr = new int[n / 2];
-        if (n % 2 != 0) {
-            for (int i = 0; i < n; i++) {
-                arr[i] = n;
-                Arrays.sort(arr);
-                System.out.println(Arrays.toString(arr));
+    public int[] solution(int n) {
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n / 2];
+        for (int i = 1; i <= n; i++) {
+            arr1[i] = i++;
+            if (i % 2 != 0) {
+                arr2[i] = i;
             }
         }
-        int[] answer = arr;
+        int[] answer = arr2;
         return answer;
     }
-}}
+}
